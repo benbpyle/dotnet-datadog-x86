e82bc452a560ae55275bb3183060d8462f0d5fdc
Benjamen Pyle <benbpyle@gmail.com>
2024-03-13 14:12:17 -0500

    adjust for rate

--- ReferenceArchitecture/ServiceA/Program.cs
+++ ReferenceArchitecture/ServiceA/Program.cs
@@ -1,15 +1,32 @@
 using System;
+using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    rateLimiterOptions.AddFixedWindowLimiter("fixed-window", fixedWindowOptions =>
+    {
+        fixedWindowOptions.Window = TimeSpan.FromSeconds(60);
+        fixedWindowOptions.PermitLimit = 20;
+        fixedWindowOptions.QueueLimit = 10;
+        fixedWindowOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+});
+
 var app = builder.Build();
 app.UseRouting();
 app.UseCors();
 app.MapControllers();
+
+app.UseRateLimiter();
 
 app.Run();
 
